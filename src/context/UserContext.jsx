3cf259d629f4ea0f3e739d23a593b3c17b8f617e
Deepak <deepak.mn2004@gmail.com>
2025-07-11 00:41:56 -0800

    Update UserContext.jsx

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,27 +1,34 @@
-import React, { createContext, useState } from 'react'
-import {food_items} from '../food'
-export const dataContext=createContext()
-const UserContext = ({children}) => {
+import { createContext, useState } from 'react'
+import { food_items } from '../food'
+// eslint-disable-next-line react-refresh/only-export-components
+export const dataContext = createContext()
+// eslint-disable-next-line react/prop-types
+const UserContext = ({ children }) => {
   const [input, setInput] = useState("")
   const [cate, setCate] = useState(food_items)
   const [showCart, setShowCart] = useState(false)
-  let data={
-input,
-setInput,
-cate,
-setCate,
-showCart,
-setShowCart,
+  let data = {
+    input,
+    setInput,
+    cate,
+    setCate,
+    showCart,
+    setShowCart,
   }
   return (
     <div>
       <dataContext.Provider value={data}>
         {children}
       </dataContext.Provider>
-      
+
     </div>
   )
 }
 
 
 export default UserContext
+
+
+
+
+
